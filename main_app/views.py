dbf24c9fbec9fff7f20ed94d27be0874f3b5afe6
vvillbii <will.bowles90@gmail.com>
2022-02-09 17:08:26 -0500

    CRUDable artist

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Artist
+from django.shortcuts import redirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -18,3 +22,25 @@ class ArtistList(TemplateView):
         context = super().get_context_data(**kwargs)
         context["painters"] = Artist.objects.all()
         return context
+
+class ArtistDetail(DetailView):
+    model = Artist
+    template_name = "artist_detail.html"
+
+class ArtistCreate(CreateView):
+    model = Artist
+    fields = ['name', 'image', 'period']
+    template_name = "artist_create.html"
+    success_url = '/artists/'
+
+class ArtistUpdate(UpdateView):
+    model = Artist
+    fields = ['name', 'image', 'period']
+    template_name = "artist_update.html"
+    def get_success_url(self):
+        return reverse('artist_detail', kwargs={'pk': self.object.pk})
+
+class ArtistDelete(DeleteView):
+    model = Artist
+    template_name = 'artist_delete_confirmation.html'
+    success_url = '/artists/'
